d125a74e831a0e035f370b3459452d63dabb848b
Puneet <birdipuneet@gmail.com>
2020-06-05 15:21:46 -0400

    Finished

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+export const About = () => {
+   return (
+      <Fragment>
+         <h1>About This App</h1>
+         <p>App to search Github users. This was based on a Udemy course from Brad Traversy.</p>
+         <p>Version: 1.0.0</p>
+      </Fragment>
+   )
+}
+
+export default About;
